71b746c290446762294bccd834608f929820488b
Kartikey Rana <44106280+kartikey09@users.noreply.github.com>
2026-03-07 21:44:46 +0530

    app updates

--- src/main/java/org/example/ScientificCalculator.java
+++ src/main/java/org/example/ScientificCalculator.java
@@ -46,4 +46,18 @@ public class ScientificCalculator {
     public double cosine(double angleRadians) {
         return Math.cos(angleRadians);
     }
+
+    public long factorial(int x) {
+        if (x < 0) return -1; // Error case for negative numbers
+        long result = 1;
+        for (int i = 1; i <= x; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    // 3. Natural Log (ln(x))
+    public double logarithm(double x) {
+        return Math.log(x);
+    }
 }
